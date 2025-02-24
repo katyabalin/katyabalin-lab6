475bdb2a5fa00d2d7e243dbd256c6d375ac3da05
katyabalin <katya.balin@gmail.com>
2025-02-23 19:31:35 -0500

    fixed the encryption order so that it is inner, middle, outer; and decryption outer, middle, inner

--- Enigma.java
+++ Enigma.java
@@ -19,15 +19,16 @@ public class Enigma {
     public String encrypt(String message) {
         StringBuilder result = new StringBuilder();
         for (char c : message.toCharArray()) {
+            if (c != '#' && (c < 'A' || c > 'Z')) {
+                result.append(c); 
+                continue;
+            }
             int index = rotors[0].indexOf(c);
-            char step1 = rotors[0].charAt(index);
-
+            char step1 = rotors[1].charAt(index);
             int index2 = rotors[1].indexOf(step1);
-            char step2 = rotors[1].charAt(index2);
-
+            char step2 = rotors[2].charAt(index2);
             int index3 = rotors[2].indexOf(step2);
             char step3 = rotors[2].charAt(index3);
-
             result.append(step3);
             rotate();
         }
@@ -37,15 +38,16 @@ public class Enigma {
     public String decrypt(String message) {
         StringBuilder result = new StringBuilder();
         for (char c : message.toCharArray()) {
+            if (c != '#' && (c < 'A' || c > 'Z')) {
+                result.append(c); 
+                continue;
+            }
             int index3 = rotors[2].indexOf(c);
             char step3 = rotors[2].charAt(index3);
-
             int index2 = rotors[1].indexOf(step3);
             char step2 = rotors[1].charAt(index2);
-
             int index1 = rotors[0].indexOf(step2);
             char step1 = rotors[0].charAt(index1);
-
             result.append(step1);
             rotate();
         }
