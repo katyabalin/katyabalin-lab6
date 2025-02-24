779f4c8654dab74b0b94d053a40e97a59c9ac724
katyabalin <katya.balin@gmail.com>
2025-02-23 19:15:41 -0500

    fixed encryption/decryption errors

--- Enigma.java
+++ Enigma.java
@@ -1,57 +1,56 @@
-public class Enigma{
-
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
+public class Enigma {
+    private static final String[] ROTOR_PATTERNS = {
+        "#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
         "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
+        "#TGOWHLIFMCSZYRVXQABUPEJKND"
+    };
 
+    private Rotor[] rotors;
 
-    private Rotor rotors[];
-        
-    public Enigma(int id1, int id2, int id3, String start){
-
+    public Enigma(int id1, int id2, int id3, String start) {
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
-        rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
-        rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
-        
+        rotors[0] = new Rotor(ROTOR_PATTERNS[id1 - 1], start.charAt(0));
+        rotors[1] = new Rotor(ROTOR_PATTERNS[id2 - 1], start.charAt(1));
+        rotors[2] = new Rotor(ROTOR_PATTERNS[id3 - 1], start.charAt(2));
     }
 
-
-    public String decrypt(String message){        
+    public String encrypt(String message) {
         StringBuilder result = new StringBuilder();
         for (char c : message.toCharArray()) {
-            char step1 = rotors[2].charAt(rotors[2].indexOf(c));
-            char step2 = rotors[1].charAt(rotors[1].indexOf(step1));
-            char step3 = rotors[0].charAt(rotors[0].indexOf(step2));
+            int index = rotors[0].indexOf(c);
+            char step1 = rotors[0].charAt(index);
+            int index2 = rotors[1].indexOf(step1);
+            char step2 = rotors[1].charAt(index2);
+            int index3 = rotors[2].indexOf(step2);
+            char step3 = rotors[2].charAt(index3);
             result.append(step3);
             rotate();
         }
         return result.toString();
     }
 
-
-    
-    public String encrypt(String message){
+    public String decrypt(String message) {
         StringBuilder result = new StringBuilder();
         for (char c : message.toCharArray()) {
-            char step1 = rotors[0].charAt(rotors[0].indexOf(c));
-            char step2 = rotors[1].charAt(rotors[1].indexOf(step1));
-            char step3 = rotors[2].charAt(rotors[2].indexOf(step2));
-            result.append(step3);
+            int index3 = rotors[2].indexOf(c);
+            char step3 = rotors[2].charAt(index3);
+            int index2 = rotors[1].indexOf(step3);
+            char step2 = rotors[1].charAt(index2);
+            int index1 = rotors[0].indexOf(step2);
+            char step1 = rotors[0].charAt(index1);
+            result.append(step1);
             rotate();
         }
         return result.toString();
     }
 
-    
-    private void rotate(){
-        if(rotors[0].rotate()){
-            if(rotors[1].rotate()){
+    private void rotate() {
+        if (rotors[0].rotate()) {
+            if (rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
     }
-    
 }
