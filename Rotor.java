b9d5cd4c584f3b824e71a3643e2f82466a01c032
katyabalin <katya.balin@gmail.com>
2025-02-23 23:50:52 -0500

    Update Rotor.java

--- Rotor.java
+++ Rotor.java
@@ -1,5 +1,5 @@
 public class Rotor {
-
+    
     private String rotorValues;
     private int position;
 
@@ -8,11 +8,19 @@ public class Rotor {
         this.position = rotorValues.indexOf(c);
     }
 
+    /**
+     * Rotates the rotor one step forward.
+     * @return true if a full rotation happened (for cascading effect).
+     */
     public boolean rotate() {
         position = (position + 1) % rotorValues.length();
         return position == 0;
     }
 
+    /**
+     * Finds the index of a given character in the rotor's mapping.
+     * Adjusts for rotation.
+     */
     public int indexOf(char c) {
         int originalIndex = rotorValues.indexOf(c);
         if (originalIndex == -1) {
@@ -21,10 +29,16 @@ public class Rotor {
         return (originalIndex - position + rotorValues.length()) % rotorValues.length();
     }
 
+    /**
+     * Gets the character at a given index, adjusted for rotation.
+     */
     public char charAt(int idx) {
         return rotorValues.charAt((idx + position) % rotorValues.length());
     }
 
+    /**
+     * Debug: Prints rotor state.
+     */
     public void printRotorState() {
         System.out.println("Rotor Position: " + position + " | Current Char: " + rotorValues.charAt(position));
     }
