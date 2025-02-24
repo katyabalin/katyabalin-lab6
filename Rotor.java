eaf7e507e0d2fa78567ca25926f1f8ff809227f9
katyabalin <katya.balin@gmail.com>
2025-02-23 22:39:41 -0500

    Update Rotor.java

--- Rotor.java
+++ Rotor.java
@@ -1,13 +1,15 @@
 public class Rotor {
 
-    private String rotorValues;  
-    private char startChar;  
-    private int position;  
+    private String rotorValues;
+    private char startChar;
+    private int position;
 
     public Rotor(String v, char c) {
         this.rotorValues = v;
         this.startChar = c;
         this.position = rotorValues.indexOf(startChar);
+        System.out.println("Rotor initialized: " + rotorValues);
+        System.out.println("Start position: " + startChar + " at index " + position);
     }
 
     /**
@@ -16,24 +18,31 @@ public class Rotor {
      */
     public boolean rotate() {
         position = (position + 1) % rotorValues.length();
-        return (rotorValues.charAt(position) == startChar);
+        boolean fullRotation = (rotorValues.charAt(position) == startChar);
+        System.out.println("Rotor rotated: New position " + rotorValues.charAt(position) + " | Full rotation: " + fullRotation);
+        return fullRotation;
     }
 
     /**
-     * Returns the **positionally shifted** index of a given character.
+     * Finds the index of a given character in the rotated rotor.
      */
     public int indexOf(char c) {
         int originalIndex = rotorValues.indexOf(c);
-        if (originalIndex == -1) return -1;  
-
-        // FIXED: Ensure index lookup is adjusted based on rotation
-        return (originalIndex - position + rotorValues.length()) % rotorValues.length();
+        if (originalIndex == -1) {
+            System.out.println("Error: Character " + c + " not found in rotor!");
+            return -1;
+        }
+        int adjustedIndex = (originalIndex - position + rotorValues.length()) % rotorValues.length();
+        System.out.println("indexOf: " + c + " is at " + originalIndex + ", adjusted for rotation: " + adjustedIndex);
+        return adjustedIndex;
     }
 
     /**
-     * Returns the character at a given index, **accounting for rotation**.
+     * Returns the character at a given index, considering the rotor's rotation.
      */
     public char charAt(int idx) {
-        return rotorValues.charAt((idx + position) % rotorValues.length());
+        char result = rotorValues.charAt((idx + position) % rotorValues.length());
+        System.out.println("charAt: Index " + idx + " maps to " + result);
+        return result;
     }
 }
