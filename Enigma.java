d3d53e046601f70a6b51bbed4f55ba206fb230a1
katyabalin <katya.balin@gmail.com>
2025-02-23 22:21:52 -0500

    added print statements to debug

--- Enigma.java
+++ Enigma.java
@@ -11,6 +11,10 @@ public class Enigma {
     private Rotor[] rotors;
 
     public Enigma(int id1, int id2, int id3, String start) {
+        System.out.println("\n[Initializing Enigma]");
+        System.out.println("Selected Rotors: " + id1 + ", " + id2 + ", " + id3);
+        System.out.println("Start Positions: " + start);
+
         rotors = new Rotor[3];
         rotors[0] = new Rotor(rotorInit[id1 - 1], start.charAt(0));
         rotors[1] = new Rotor(rotorInit[id2 - 1], start.charAt(1));
@@ -19,46 +23,19 @@ public class Enigma {
 
     /** Encrypts the given message */
     public String encrypt(String message) {
+        System.out.println("\n[Encrypting Message] Input: " + message);
         StringBuilder result = new StringBuilder();
 
         for (char c : message.toCharArray()) {
-            int index1 = rotors[0].indexOf(c);
-            if (index1 == -1) continue; // Skip unrecognized characters
+            System.out.println("\nEncrypting: " + c);
 
+            int index1 = rotors[0].indexOf(c);
+            if (index1 == -1) {
+                System.out.println("Error: Character '" + c + "' not found in rotor.");
+                continue;
+            }
             char step1 = rotors[1].charAt(index1);
             int index2 = rotors[1].indexOf(step1);
             char step2 = rotors[2].charAt(index2);
 
-            result.append(step2);
-            rotate();
-        }
-        return result.toString();
-    }
-
-    /** Decrypts the given message */
-    public String decrypt(String message) {
-        StringBuilder result = new StringBuilder();
-
-        for (char c : message.toCharArray()) {
-            int index2 = rotors[2].indexOf(c);
-            if (index2 == -1) continue; // Skip unrecognized characters
-
-            char step2 = rotors[1].charAt(index2);
-            int index1 = rotors[1].indexOf(step2);
-            char step1 = rotors[0].charAt(index1);
-
-            result.append(step1);
-            rotate();
-        }
-        return result.toString();
-    }
-
-    /** Rotates the rotors */
-    private void rotate() {
-        if (rotors[0].rotate()) {
-            if (rotors[1].rotate()) {
-                rotors[2].rotate();
-            }
-        }
-    }
-}
+            System.out.println("Step 1: " + c + " -> "
