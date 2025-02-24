41a392abc8784a8113e1b38824e5064ceb417b15
katyabalin <katya.balin@gmail.com>
2025-02-23 19:47:25 -0500

    correct rotate function

--- Enigma.java
+++ Enigma.java
@@ -1,5 +1,6 @@
 public class Enigma {
-    private static final String[] ROTOR_PATTERNS = {
+
+    private final String[] rotorInit = {
         "#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
@@ -15,38 +16,38 @@ public class Enigma {
         System.out.println("Start Positions: " + start);
 
         rotors = new Rotor[3];
-        rotors[0] = new Rotor(ROTOR_PATTERNS[id1 - 1], start.charAt(0));
-        rotors[1] = new Rotor(ROTOR_PATTERNS[id2 - 1], start.charAt(1));
-        rotors[2] = new Rotor(ROTOR_PATTERNS[id3 - 1], start.charAt(2));
+        rotors[0] = new Rotor(rotorInit[id1 - 1], start.charAt(0));
+        rotors[1] = new Rotor(rotorInit[id2 - 1], start.charAt(1));
+        rotors[2] = new Rotor(rotorInit[id3 - 1], start.charAt(2));
     }
 
+    /** Encrypts the given message */
     public String encrypt(String message) {
         System.out.println("\n[Encrypting Message] Input: " + message);
         StringBuilder result = new StringBuilder();
-        
+
         for (char c : message.toCharArray()) {
             System.out.println("\nEncrypting: " + c);
 
-            // Encoding through the rotors in order
+            // Step 1: Inner rotor to middle rotor
             int index1 = rotors[0].indexOf(c);
             char step1 = rotors[1].charAt(index1);
             System.out.println("Step 1: " + c + " -> " + step1 + " (Index: " + index1 + ")");
 
+            // Step 2: Middle rotor to outer rotor
             int index2 = rotors[1].indexOf(step1);
             char step2 = rotors[2].charAt(index2);
             System.out.println("Step 2: " + step1 + " -> " + step2 + " (Index: " + index2 + ")");
 
-            int index3 = rotors[2].indexOf(step2);
-            char step3 = rotors[2].charAt(index3);
-            System.out.println("Final Encrypted Output: " + step3 + " (Index: " + index3 + ")");
-
-            result.append(step3);
+            result.append(step2);
             rotate();
         }
+
         System.out.println("\n[Encryption Complete] Output: " + result.toString());
         return result.toString();
     }
 
+    /** Decrypts the given message */
     public String decrypt(String message) {
         System.out.println("\n[Decrypting Message] Input: " + message);
         StringBuilder result = new StringBuilder();
@@ -54,37 +55,36 @@ public class Enigma {
         for (char c : message.toCharArray()) {
             System.out.println("\nDecrypting: " + c);
 
-            // Decoding through the rotors in reverse order
-            int index3 = rotors[2].indexOf(c);
-            char step3 = rotors[2].charAt(index3);
-            System.out.println("Step 3: " + c + " -> " + step3 + " (Index: " + index3 + ")");
-
-            int index2 = rotors[1].indexOf(step3);
+            // Step 1: Outer rotor to middle rotor
+            int index2 = rotors[2].indexOf(c);
             char step2 = rotors[1].charAt(index2);
-            System.out.println("Step 2: " + step3 + " -> " + step2 + " (Index: " + index2 + ")");
+            System.out.println("Step 1: " + c + " -> " + step2 + " (Index: " + index2 + ")");
 
-            int index1 = rotors[0].indexOf(step2);
+            // Step 2: Middle rotor to inner rotor
+            int index1 = rotors[1].indexOf(step2);
             char step1 = rotors[0].charAt(index1);
-            System.out.println("Final Decrypted Output: " + step1 + " (Index: " + index1 + ")");
+            System.out.println("Step 2: " + step2 + " -> " + step1 + " (Index: " + index1 + ")");
 
             result.append(step1);
             rotate();
         }
+
         System.out.println("\n[Decryption Complete] Output: " + result.toString());
         return result.toString();
     }
 
+    /** Rotates the rotors */
     private void rotate() {
-        System.out.println("Before Rotation: " + rotors[0].charAt(0) + " " + rotors[1].charAt(0) + " " + rotors[2].charAt(0));
-        
-        boolean rotateNext = rotors[0].rotate();
-        if (rotateNext) {
-            rotateNext = rotors[1].rotate();
-            if (rotateNext) {
+        System.out.println("\nBefore Rotation: " + rotors[0].currentChar() + " " +
+            rotors[1].currentChar() + " " + rotors[2].currentChar());
+
+        if (rotors[0].rotate()) {
+            if (rotors[1].rotate()) {
                 rotors[2].rotate();
             }
         }
 
-        System.out.println("After Rotation: " + rotors[0].charAt(0) + " " + rotors[1].charAt(0) + " " + rotors[2].charAt(0));
+        System.out.println("After Rotation: " + rotors[0].currentChar() + " " +
+            rotors[1].currentChar() + " " + rotors[2].currentChar());
     }
 }
