e786d2c4b749932b1fdf236d38855d85be5354cf
katyabalin <katya.balin@gmail.com>
2025-02-23 22:16:03 -0500

    fixed the rotation

--- Rotor.java
+++ Rotor.java
@@ -2,27 +2,36 @@ public class Rotor {
 
     private String rotorValues;
     private char startChar;
-    private int currentPosition;
+    private int position;
 
-    public Rotor(String v, char c){
+    public Rotor(String v, char c) {
         this.rotorValues = v;
         this.startChar = c;
-        this.currentPosition = rotorValues.indexOf(c);
+        this.position = rotorValues.indexOf(startChar);
     }
 
-    // Rotate the rotor one position clockwise
-    public boolean rotate(){
-        currentPosition = (currentPosition + 1) % rotorValues.length();
-        return rotorValues.charAt(currentPosition) == startChar;
+    /**
+     * Rotates the rotor one step clockwise.
+     * Returns true if the rotor completes a full cycle back to the start position.
+     */
+    public boolean rotate() {
+        position = (position + 1) % rotorValues.length();
+        return rotorValues.charAt(position) == startChar;
     }
 
-    // Get the index of a given character in the rotor
-    public int indexOf(char c){
-        return rotorValues.indexOf(c);
+    /**
+     * Finds the index of a given character in the rotor, adjusted for rotation.
+     */
+    public int indexOf(char c) {
+        int originalIndex = rotorValues.indexOf(c);
+        if (originalIndex == -1) return -1;  // Character not found
+        return (originalIndex - position + rotorValues.length()) % rotorValues.length();
     }
 
-    // Get the character at a specific index in the rotor
-    public char charAt(int idx){
-        return rotorValues.charAt((idx + currentPosition) % rotorValues.length());
+    /**
+     * Returns the character at a given index, considering the rotor's rotation.
+     */
+    public char charAt(int idx) {
+        return rotorValues.charAt((idx + position) % rotorValues.length());
     }
 }
