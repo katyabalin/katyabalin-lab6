04e69327badeee462c45165a6c21574284bd8915
katyabalin <katya.balin@gmail.com>
2025-02-23 19:46:08 -0500

    Update Comms.java for testing

--- Comms.java
+++ Comms.java
@@ -1,36 +1,29 @@
-import java.util.Scanner;
-
-/**
- * Creates an Enigma machine based on command-line arguments, and reads the message
- * from the user on STDIN. Decrypts/Encrypts using the enigma class.
- */
 public class Comms {
+    public static String run(String[] args) {
+        boolean encrypt = args[4].equals("encrypt");
 
-  public static String run(String[] args) {
-    
-    // Encrypt or decrypt argument
-    boolean encrypt = true;
-    if( args[4].equals("decrypt") )
-      encrypt = false;
+        int id1 = Integer.parseInt(args[0]);
+        int id2 = Integer.parseInt(args[1]);
+        int id3 = Integer.parseInt(args[2]);
 
-    // Rotors
-    int id1 = Integer.parseInt(args[0]);
-    int id2 = Integer.parseInt(args[1]);
-    int id3 = Integer.parseInt(args[2]);
-    
-    // Get the message from the user, and call the appropriate method
-    // Scanner scan = new Scanner(System.in);
-    // String message = scan.next();
+        String message = args[5];
 
-    String message = args[5];
+        Enigma enigma = new Enigma(id1, id2, id3, args[3]);
 
-    // Call the Enigma's constructor to build the machine
-    Enigma enigma = new Enigma(id1, id2, id3, args[3]);
+        if (encrypt)
+            return enigma.encrypt(message);
+        else
+            return enigma.decrypt(message);
+    }
 
-    // Encrypt or Decrypt
-    if( encrypt )
-      return enigma.encrypt(message);
-    else
-      return enigma.decrypt(message);
-  }
+    // Add this main method for manual testing
+    public static void main(String[] args) {
+        if (args.length < 6) {
+            System.out.println("Usage: java Comms <id1> <id2> <id3> <start> <encrypt|decrypt> <message>");
+            return;
+        }
+
+        String result = run(args);
+        System.out.println("\n[Result]: " + result);
+    }
 }
